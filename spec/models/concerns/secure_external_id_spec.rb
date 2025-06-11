74b807850eac55b4c5dc330f7c60948d86344ead
Ershad Kunnakkadan <ershad92@gmail.com>
2025-06-11 15:59:36 -0400

    Refactor

--- spec/models/concerns/secure_external_id_spec.rb
+++ spec/models/concerns/secure_external_id_spec.rb
@@ -36,17 +36,18 @@ RSpec.describe SecureExternalId do
     it "generates an encrypted token" do
       token = test_instance.secure_external_id(scope: "test")
       expect(token).to be_a(String)
-      expect(token).not_to include(test_instance.id.to_s)
+      expect(token.length).to be >= 50
     end
 
-    it "includes expiry when provided" do
-      expires_at = 1.hour.from_now
-      token = test_instance.secure_external_id(scope: "test", expires_at: expires_at)
-      expect(token).to be_a(String)
+    it "raises error when primary key not found" do
+      allow(GlobalConfig).to receive(:dig).with(:secure_external_id, default: {}).and_return({
+        primary_key_version: "2",
+        keys: { "1" => "a" * 32 }
+      })
 
-      travel_to 2.hours.from_now do
-        expect(test_class.find_by_secure_external_id(token, scope: "test")).to be_nil
-      end
+      expect {
+        test_instance.secure_external_id(scope: "test")
+      }.to raise_error(SecureExternalId::KeyNotFound)
     end
   end
 
@@ -65,51 +66,43 @@ RSpec.describe SecureExternalId do
       expect(test_class.find_by_secure_external_id(token, scope: "wrong")).to be_nil
     end
 
-    it "returns nil for expired token" do
+    it "checks for expired token" do
       expires_at = 1.hour.from_now
       token = test_instance.secure_external_id(scope: "test", expires_at: expires_at)
 
+      travel_to 45.minutes.from_now do
+        expect(test_class.find_by_secure_external_id(token, scope: "test")).to be_a(test_class)
+      end
+
       travel_to 2.hours.from_now do
-        expect(test_class.find_by_secure_external_id(token, scope: "test")).to be_nil
+        expect(test_class.find_by_secure_external_id(token, scope: "test")).to be_nil # expired
       end
     end
-  end
 
-  describe ".encrypt_id" do
-    it "raises KeyNotFound when primary key not found" do
-      allow(GlobalConfig).to receive(:dig).with(:secure_external_id, default: {}).and_return({
-        primary_key_version: "2",
-        keys: { "1" => "a" * 32 }
-      })
 
-      expect {
-        test_class.encrypt_id(123, scope: "test")
-      }.to raise_error(SecureExternalId::KeyNotFound)
-    end
-  end
-
-  describe ".decrypt_id" do
     it "returns nil for non-string input" do
-      expect(test_class.decrypt_id(123, scope: "test")).to be_nil
+      expect(test_class.find_by_secure_external_id(123, scope: "test")).to be_nil
     end
 
     it "returns nil for invalid base64" do
-      expect(test_class.decrypt_id("invalid base64!", scope: "test")).to be_nil
+      expect(test_class.find_by_secure_external_id("invalid base64!", scope: "test")).to be_nil
     end
 
     it "returns nil for wrong model name" do
       other_class = Class.new do
         include SecureExternalId
+
         def self.name
           "OtherClass"
         end
+
         def id
           123
         end
       end
 
       token = test_instance.secure_external_id(scope: "test")
-      expect(other_class.decrypt_id(token, scope: "test")).to be_nil
+      expect(other_class.find_by_secure_external_id(token, scope: "test")).to be_nil
     end
   end
 end
